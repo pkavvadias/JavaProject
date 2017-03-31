9e5fa9b907d9df7db39570248807448f1b9b3f70
PANAGIOTIS KAVADIAS <pkavadia13@gmail.com>
2017-03-31 21:18:51 +0300

    Added retrieveFromNumber method in Hotel class

--- src/Hotel.java
+++ src/Hotel.java
@@ -2,11 +2,25 @@ import java.util.ArrayList;
 
 public class Hotel {
     String HotelName;
-    ArrayList<Room> rooms=new ArrayList<Room>();
-    ArrayList<Reservation> reservations=new ArrayList<Reservation>();
+    ArrayList<Room> rooms = new ArrayList<Room>();
+    ArrayList<Reservation> reservations = new ArrayList<Reservation>();
 
-    public void addRoom(Room r)
-    {
+    public void addRoom(Room r) {
         rooms.add(r);
+    }
+
+    public Room retrieveFromNumber(int id) {
+        Room c = new Room();
+        for (int i = 0; i==rooms.size(); i++) { //For statement doesnt loop warning.Possible bug to check
+            c = rooms.get(i);
+            if (c.RoomNumber == id) {
+                return c;
+            } else {
+                c = null;
+                return c;
+            }
+
+        }
+        return c;
     }
 }
