477d5e67989e17d1072a84f7da04c76164619548
PANAGIOTIS KAVADIAS <pkavadia13@gmail.com>
2017-03-31 18:53:30 +0300

    Completed class RoomTypeA+fixed a nasty typo

--- src/RoomTypeA.java
+++ src/RoomTypeA.java
@@ -1,5 +1,26 @@
 
 public class RoomTypeA extends Room{
     double PricePerDay;
+@Override
+    public double costCalculate()
+{
+double cost=0;
+    for(int i=0;i<31;i++)
+    {
+        if(Availability[i]==null)
+        {
 
+            cost +=0;
+        }
+        else
+        {
+            for(;Availability[i]==Availability[30];)
+            {
+                cost+=(PricePerDay*PricePerPerson);
+            }
+        }
+    }
+    return cost;
 }
+}
+
