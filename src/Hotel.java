2428e555dd53f0afca83657f7682b60edbc32068
PANAGIOTIS KAVADIAS <pkavadia13@gmail.com>
2017-04-01 10:57:20 +0300

    Added method retrieveReservationFromNumber and done some refactoring on retrieveRoomFromNumber method that fixed some warnings

--- src/Hotel.java
+++ src/Hotel.java
@@ -5,24 +5,39 @@ public class Hotel {
     String HotelName;
     ArrayList<Room> rooms = new ArrayList<Room>();
     ArrayList<Reservation> reservations = new ArrayList<Reservation>();
-
     public void addRoom(Room r) {
         rooms.add(r);
     }
 
-    public Room retrieveFromNumber(int id) {
-        Room c = new Room();
+    public Room retrieveRoomFromNumber(int id) {
+        Room c;
+        Room a=new Room();
         Iterator it=rooms.iterator();
-        for (int i = -0; it.hasNext(); i++) { //For statement doesnt loop warning.Possible bug to check
+        for (int i = 0; it.hasNext(); i++) {
             c = rooms.get(i);
             if (c.RoomNumber == id) {
-                return c;
+                a=c;
             } else {
-                c = null;
-                return c;
+                a = null;
             }
 
         }
-        return c;
+        return a;
+    }
+    public Reservation retrieveResevationFromNumber(int id)
+    {
+        Reservation r;
+        Reservation d=new Reservation();
+        Iterator itr=reservations.iterator();
+        for (int i = 0; itr.hasNext(); i++) { 
+            r = reservations.get(i);
+            if (r.ReservationNumber == id) {
+                d=r;
+            } else {
+                d=null;
+            }
+
+        }
+        return d;
     }
 }
