42024e10329db5d405a29bb84e53e7e3a4664f23
PANAGIOTIS KAVADIAS <pkavadia13@gmail.com>
2017-04-02 17:02:41 +0300

    Added the loop and the menu options.Still some work to finish main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
+import java.util.Random;
+
 
 public class Main {
     public static void main(String[] args) {
         Hotel hotel = new Hotel();
+        Random random = new Random(); // just create one and keep it around
+        //First names taken from wikipedia's list of most common names in Greece
+        String firstNames []={"George","Giannis","Konstantinos","Kostas","Dimitris","Nikos","Panagiotis","Vasilis",
+        "Christos","Thanasis","Michalis","Maria","Eleni","Vasiliki","Sofia","Aggeliki","Dimitra","Kwstantina"};
+        //Last names taken from wikipedia's list of most common last names in Greece
+        String lastNames[]={"Papadopoulos","Vlachos","Angelopoulos","Nikolaidis","Georgiou","Athanasiadis",
+        "Dimitriadis","Papadakis","Panagiotopoulos","Antoniou","Papantoniou","Petridis"};
+
 
         //Block for first room
         {
@@ -82,6 +92,28 @@ public class Main {
             room10.PricePerDay=30;
             room10.MaxCapacity=3;
             hotel.rooms.add(room10);
+        }
+        for(int i=0;;i++)
+        {
+        String fullName=firstNames[random.nextInt(firstNames.length)] + " " + lastNames[random.nextInt(lastNames.length)];
+        Reservation reservation=new Reservation();
+        reservation.Arrival=random.nextInt(30-1)+1;//Sets random values between 1 and 30
+        reservation.DaysOfStay=random.nextInt(31-reservation.Arrival);
+        //App supports only one month so if if DaysofStay+Arrival>30 a crash will occur
+        reservation.NumberOfPeople=random.nextInt(6-1)+1;
+        //Maximum number of people is 6 as no room supports more than 6
+        hotel.addReservationToFirstRoom(reservation);
+        hotel.reservations.add(reservation);
+        //The following code will have to be changed if/when i add a swing guif
+        System.out.println("Press 1 for next repeat(programm continues");
+        System.out.println("Press 2 to add a reservation");
+        System.out.println("Press 3 to cancel a reservation");
+        System.out.println("Press 4 to view reservations");
+        System.out.println("Press 5 to view rooms");
+        System.out.println("Press 6 to view reservation plan");
+        System.out.println("Press 7 to view income");
+        System.out.println("Press any other key to exit");
+
         }
     }
 }
